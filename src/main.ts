d9481e394c5972bcc54c6c1a51ca71ed2e3187de
vilkasts <vilkasts@gmail.com>
2024-11-26 00:24:31 +0100

    fix: update logger

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { LoggerService } from './tools/logger/logger.service';
-import { ExceptionsFilter } from './tools/exception/exception.filter';
+import { ExceptionsFilter } from './tools/exceptions-filter/exceptions-filter';
 
 async function bootstrap() {
   BigInt.prototype['toJSON'] = function () {
@@ -13,6 +13,42 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const loggerService = app.get(LoggerService);
   app.useLogger(loggerService);
+
+  app.use((req: any, res: any, next: () => void) => {
+    const { method, url } = req;
+    const chunks: Buffer[] = [];
+    let bodyData = '';
+
+    req.on('data', (chunk: any) => (bodyData += chunk));
+
+    const send = res.send;
+
+    res.send = function (chunk: any) {
+      if (chunk)
+        chunks.push(Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk));
+      return send.apply(res, [chunk]);
+    };
+
+    res.on('finish', () => {
+      const logMessage = `
+      \nRequest: {url: ${url}, method: ${method}, body: ${JSON.stringify(
+        JSON.parse(bodyData),
+        null,
+        0,
+      )}}
+      \nResponse: {status: ${res.statusCode}, body: ${Buffer.concat(
+        chunks,
+      ).toString('utf-8')}}`;
+
+      if (res.statusCode >= 400) {
+        loggerService.error(logMessage, 'HTTP');
+      } else {
+        loggerService.log(logMessage, 'HTTP');
+      }
+      console.log(logMessage);
+    });
+    next();
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Home Library Service API')
